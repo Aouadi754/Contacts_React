2d620412a09a9011fde43d0254b09b896f5b0ede
wassim mokhtar <wassimmokhtar92@gmail.com>
2020-10-23 13:31:16 +0100

    add-Contact

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {Button} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,13 +7,15 @@ import Add from './Add';
 
 
 function App() {
+  const [UserContact, SetUserContact] = useState("")
+  const AddNewContact = (x) => SetUserContact([...UserContact, x]);
   return (
     <div className='List'>
     <div className="App">
      <Link to='/contact list'><Button variant="primary">contact list</Button> </Link> 
      <Link to='/add contact'> <Button variant="primary">add contact</Button></Link>
      </div>
-       <Add/>
+       <Add AddNewContact={AddNewContact}/>
     </div>
 
   );
